f268b22f18a52fd5a9a4dfc9d3913d26ac4a85cf
connor-lepre <connor.lepre@gmail.com>
2025-07-16 20:07:09 -0400

    Ball Scores Right

--- cheap_umbrella/scripts/Goal.gd
+++ cheap_umbrella/scripts/Goal.gd
@@ -1,24 +1,33 @@
 extends Node2D
 
-@onready var goal_body = $Goal
+@onready var rim_check = $Goal/RimCheck
+@onready var net_check = $Goal/NetCheck
 @onready var score_fx = $ScoreFX
 signal ball_scored
-var balls_in_collider = {}
+
+var balls_in_rim := {}
 
 func _ready():
-	goal_body.body_entered.connect(_on_goal_body_entered)
-	goal_body.body_exited.connect(_on_goal_body_exited)
-	
-func _on_goal_body_entered(body):
-	if body.is_in_group("ball"):
-		if body.linear_velocity.y > 0:
-			balls_in_collider[body.get_instance_id()] = true
+	rim_check.body_entered.connect(_on_rim_check_entered)
+	rim_check.body_exited.connect(_on_rim_check_exited)
+	net_check.body_entered.connect(_on_net_check_entered)
 
-func _on_goal_body_exited(body):
+func _on_rim_check_entered(body):
+	if body.is_in_group("ball") and body.linear_velocity.y > 0:
+		balls_in_rim[body.get_instance_id()] = true
+		print("Body in rim check:", body)
+
+func _on_rim_check_exited(body):
 	var id = body.get_instance_id()
-	if id in balls_in_collider and balls_in_collider[id]:
+	if id in balls_in_rim:
+		balls_in_rim.erase(id)
+
+func _on_net_check_entered(body):
+	var id = body.get_instance_id()
+	if id in balls_in_rim and body.linear_velocity.y > 0:
 		emit_signal("ball_scored", body)
-		print("Ball scored")
-		score_fx.emitting = false
-		score_fx.emitting = true
-		balls_in_collider.erase(id)
+		print("ball scored")
+		if score_fx:
+			score_fx.emitting = false
+			score_fx.emitting = true
+		balls_in_rim.erase(id)
